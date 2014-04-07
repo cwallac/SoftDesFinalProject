b51051c3e845dc873fd95d49a7475ed6b5e62efe
Deniz Celik <dcelik@TurkishDelight.(none)>
2014-04-07 03:44:21 -0400

    created example gui

---
+++ gui.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  7 03:21:32 2014
+
+@author: dcelik
+"""
+
+import Tkinter as tk
+
+class gui(tk.Tk):
+    def __init__(self,parent):
+        tk.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+        
+        self.bbcanvas=tk.Canvas(self.parent,bg='black')
+        self.scanvas=tk.Canvas(self.parent,bg='red')
+
+        self.bbcanvas.grid(column=0,row=0,sticky='NS')  
+        self.scanvas.grid(column=1,row=0,sticky='NS')              
+            
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,True)
+        self.update()
+        self.geometry(self.geometry())     
+        
+
+if __name__ == "__main__":
+    app = gui(None)
+    app.title('C.I.R.C.U.I.T')
+    app.mainloop()
+    
+    
+    
