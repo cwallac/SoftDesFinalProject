228bfee0c9e5160f23235920d040a70a03a32da3
cwallac <christopher.wallace@students.olin.edu>
2014-04-17 10:32:00 -0400

    Did some work on placement

--- BreadBoardStructure.py
+++ BreadBoardStructure.py
@@ -3,6 +3,8 @@ from BreadboardModule import *
 
 #BREADBOARD[2] = left, [3] = right
 
+'''I need to add all conditions for everything only left has been done for them, some not even all possible scenarios '''
+
 def placeFirstComponent(component,breadboard):
 
 	
@@ -75,6 +77,8 @@ def componentPlacement(component,breadboard):
 								#WRITE FUNCTION TO FIND WHAT ROW THIS MATCHES AND CHENGE POSITION AND OCCUPIED VALUES GRAB INDEX
 								
 								changePosition(component,connectedComponent,[i,index],breadboard)
+
+								placeSecondPin(component,breadboard)
 								return "PLACED"
 
 def changePosition(matchComponent,placedComponent,componentPin,breadboard):
@@ -108,12 +112,71 @@ def changePosition(matchComponent,placedComponent,componentPin,breadboard):
 						else:
 							
 							component.x[1] = breadboard[3][0].xpos + k
-							breadboard[3][placedComponent.y[connect]].Occupied[k] = matchComponent
+							breadboard[3][placedComponent.y[connect]].Occupied[k] = component
 							
 							return "PLACED"
 				
 
-def placeSecondPin:
+def placeSecondPin(component,breadboard):
+	# CHANGE TO CHECK WHERE A CONNECTION IS FIRST
+	connect = component.connections[2]
+	print connect
+	for index in range(len(connect)+1):
+		print index
+		if component.x[1] <= breadboard[2][0].xpos+4 :
+
+			closestConnection = whereDaConnectionAt(breadboard[2],component)
+
+			direction = (closestConnection - component.y[1])/abs(closestConnection - component.y[1])
+			for tile in range(5):
+				if breadboard[2][component.y[1]+component.pin_gap*direction].Occupied[tile] == component: #POTENTIAL ISSUE IF WRONG PIN OF COMPONENT
+					component.y[2] = component.y[1]+component.pin_gap*direction
+					for k in range(4,-1,-1):
+						if breadboard[2][component.y[2]].Occupied[k] != False:
+							pass
+						else:
+							
+							component.x[2] = breadboard[2][0].xpos + k
+							breadboard[2][component.y[2]].Occupied[k] = component
+							
+							return "PLACED"
+
+			
+
+			
+
+			
+def whereDaConnectionAt(breadboardSide,component):
+	returnList = []
+	for row in range(30): # CHANGE TO LENGTH
+		for tile in breadboardSide[row].Occupied:
+			if breadboardSide[row].Occupied[tile] == component:
+				if row == component.y[1]:
+					
+					pass
+				else:
+					returnList.append(row)
+	if len(returnList) == 0:
+		return False
+	interList = []
+	for distance in returnList:
+		if abs(distance - component.y[1]) >= component.pin_gap and distance not in interList:
+			interList.append(distance)
+
+	distances = []
+
+	for i in interList:
+		distances.append(abs(i-component.y[1]))
+	if len(distances) == 0:
+		return False
+	index = interList.index(min(distances))
+	
+	return interList[index]
+
+
+
+
+
 
 
 
@@ -137,27 +200,32 @@ if __name__ == '__main__':
 
 	board = createBreadboard()
 
-	Resistor1 = resistor(45000,4,5,{})
-	DIP = dip(4,5,{4:[Resistor1]},'dip',number_of_pins = 8, pin_gap = 3)
+	Resistor1 = resistor(45000,4,5,{1:[],2:[]})
+	DIP = dip(4,5,{1:[Resistor1]},'dip',number_of_pins = 8, pin_gap = 3)
 	Resistor1.connections[1] = [DIP]
 	component = Resistor1
-
+	Resistor2 = resistor(5,4,4,{1:[Resistor1],2:[]})
+	Resistor1.connections[2].append(Resistor2)
 
 
 	pinOnerail = board[2][0]
 	pinTworail = board[3][0]
-	
+	board[2][3].Occupied[0] = Resistor1
+
 	placeFirstComponent(DIP,board)
 	componentPlacement(Resistor1,board)
+	componentPlacement(Resistor2,board)
 
 	
-	
+
 
 
 print DIP.y
 print Resistor1.y
 print Resistor1.x
-print board[3][1].Occupied
+print Resistor2.y
+print Resistor2.x
+print board[2][3].Occupied
 
 	
 	
