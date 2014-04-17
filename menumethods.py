8584975d642fd1999e71eb04bd4dacb674615bd6
sgubba <sgubba@ubuntu.(none)>
2014-04-17 04:09:38 -0400

    bitch pls

--- menumethods.py
+++ menumethods.py
@@ -5,5 +5,23 @@ Created on Thu Apr 17 01:29:59 2014
 @author: sgubba
 """
 
+about_messageSaveCheck = "Do you want to save your current project first?"
+
 def save():
-    
+
+def insertResistor():
+    if res_go:
+         
+def insertCapacitor():
+    if cap_go:
+        
+def insertDip():
+    if dip_go:
+        
+def inserWire():
+    if wire_go:
+        
+def createNew():
+    if fileStatus == 'unsaved':
+        msg = tk.Message(popup,text=self.about_messageSaveCheck)
+        msg.pack()
