442c9c5768b9cc0a1ca0129337637cb18aee37b1
cwallac <christopher.wallace@students.olin.edu>
2014-04-23 15:56:56 -0400

    Added conection to power object

--- ComponentModule.py
+++ ComponentModule.py
@@ -48,8 +48,9 @@ class trace(object):
 		self.value2 = value2
 
 class power(object):
-	def __init__(self,value):
+	def __init__(self,value,connections):
 		self.value = value
+		self.connections = {}
 		
 if __name__ == '__main__':
 
