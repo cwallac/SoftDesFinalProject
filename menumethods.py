9f01d79e504691763daccf8761e7acca494f53c8
sgubba <sgubba@ubuntu.(none)>
2014-04-23 03:25:29 -0400

    replicating some methods

--- menumethods.py
+++ menumethods.py
@@ -7,7 +7,13 @@ Created on Thu Apr 17 01:29:59 2014
 
 about_messageSaveCheck = "Do you want to save your current project first?"
 res_go=False
+cap_go=False
+dip_go=False
+wire_go=False
 res_coords=[]
+cap_coords=[]
+dip_coords=[]
+wire_coords=[]
 
 def insertResistor():
     global res_go
@@ -17,13 +23,27 @@ def insertResistor():
     print res_go
     
 def insertCapacitor():
-    pass#if cap_go:
-        
+    global cap_go
+    cap_go= not cap_go
+    global cap_coords
+    cap_coords = []
+    print cap_coords
+    
 def insertDip():
     pass#if dip_go:
-        
+    global dip_go
+    dip_go= not dip_go
+    global dip_coords
+    dip_coords = []
+    print dip_coords
+    
 def inserWire():
-    pass#if wire_go:
+    pass#if dip_go:
+    global wire_go
+    wire_go= not wire_go
+    global wire_coords
+    wire_coords = []
+    print wire_coords
         
 def createNew():
     pass#if fileStatus == 'unsaved':
