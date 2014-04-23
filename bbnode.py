f6ba25b547aef3af8f3fbe0125f7897bbde76689
Deniz Celik <dcelik@TurkishDelight.(none)>
2014-04-23 07:20:07 -0400

    resistors and capacitors implemented. fix multiple about windows appearing. rearranged description of bb nodes, now empty columns and rows still count when numbering rows/columns

--- bbnode.py
+++ bbnode.py
@@ -4,81 +4,30 @@ Created on Thu Apr 17 03:25:57 2014
 
 @author: dcelik
 """
-import ttk
-import menumethods
-import gui
+import Tkinter as tk
 
-class bbnode(ttk.Button):
+class bbnode(tk.Button):
     def __init__(self,frame,x,y):
-        self.xloc = self.xpixtoloc(x)
-        self.yloc = self.ypixtoloc(y)
-        ttk.Button.__init__(self,frame,style='TCheckbutton')
+        self.xloc = x/30#self.xpixtoloc(x)
+        self.yloc = (y+22)/30
+        tk.Button.__init__(self,frame)
         self.parent = frame
         self.height = 16
         self.width = 17
         self.xpix = x
         self.ypix = y
         self.place(x=self.xpix,y=self.ypix,height=self.height,width=self.width)
-        self.bind("<Button-1>",self.processMouseEvent)
         
     def getloc(self):
         return (self.xloc,self.yloc)  
-
-    def xpixtoloc(self,val):
-        v = val/30
-        if v>=3:
-            v-=1
-        if v>=8:
-            v-=1
-        if v>=13:
-            v-=1
-        return v
-        
-    def xloctopix(self,val):
-        v = val
-        if v>=3:
-            v+=1
-        if v>=9:
-            v+=1
-        if v>=15:
-            v+=1
-        v = v*30
-        return v
-        
-    def yloctopix(self,val):
-        return (val*30)-22
-        
-    def ypixtoloc(self,val):
-        return (val+22)/30
-        
-    def processMouseEvent(self, event):
-        coords= str(self.xloc) + ", " + str(self.yloc)
-        print(coords)
-        print menumethods.res_coords
-        print menumethods.res_go
-        if menumethods.res_go and len(menumethods.res_coords)<2:
-            print "added!"
-            menumethods.res_coords.append((self.xloc,self.yloc))
-        if len(menumethods.res_coords)>=2:
-            print "visual"
-            origin = menumethods.res_coords[0]
-            pixorigin=(self.xloctopix(origin[0]),self.yloctopix(origin[1]))
-            end = menumethods.res_coords[1]
-            pixend=(self.xloctopix(end[0]),self.yloctopix(end[1]))
-            menumethods.res_coords=[]
-            print origin
-            print end
-            print pixorigin
-            print pixend
-            res = self.parent.create_rectangle(pixorigin[0],pixorigin[1],pixorigin[0]+17,pixend[1]+16,fill="blue")
-            self.parent.tag_raise(res)
-            self.parent.create_rectangle(pixorigin[0]-5,pixorigin[1]-5,pixorigin[0]+17+5,pixend[1]+16+5,fill="red")
-            resbut = [i for i in gui.buttonlist]# if i.getloc()[0]<pixorigin[0]]
-            for i in resbut:
-                i.grid_forget
-#            print self.parent.find_all()            
-#            self.parent.move(3,1000,1000)
-#            self.parent.move(2,1000,1000)
-#            self.parent.move(1,1000,1000)
+#    def xpixtoloc(self,val):
+#        v = val/30
+#        if v>=3:
+#            v-=1
+#        if v>=8:
+#            v-=1
+#        if v>=13:
+#            v-=1
+#        return v    
         
         
--- menumethods.py
+++ menumethods.py
@@ -4,7 +4,50 @@ Created on Thu Apr 17 01:29:59 2014
 
 @author: sgubba
 """
+import Tkinter as tk
 
+aboutprogopen = False
+aboutcreatopen = False
+
+def aboutprog():
+    global aboutprogopen
+    if not aboutprogopen:
+        aboutprogopen = True
+        
+        popup = tk.Toplevel()
+        popup.title("About this program")
+        about_messageprog = "AN INTERACTIVE CIRCUIT DESIGN STUDIO SUITE FOR CREATION OF BREADBOARD AND SCHEMATIC PROTOTYPES."
+    
+        msg = tk.Message(popup,text=about_messageprog)
+        msg.pack()
+        
+        button = tk.Button(popup,text="Close", command = lambda: aboutkill(popup,"p"))
+        button.pack()
+
+def aboutcreat():
+    global aboutcreatopen
+    if not aboutcreatopen:
+        aboutcreatopen = True
+        
+        popup = tk.Toplevel()
+        popup.title("About the creators")
+        about_messagecreat = "CREATED BY DENIZ CELIK, SUBHASH GUBBA, CHRIS WALLACE, AND RADMER VAN DER HEYDE. ALL STUDENTS ARE FRESHMAN AT THE FRANKLIN W. OLIN COLLEGE OF ENGINEERING IN NEEDHAM, MA."        
+
+        msg = tk.Message(popup,text=about_messagecreat)
+        msg.pack()
+        
+        button = tk.Button(popup,text="Close", command = lambda: aboutkill(popup,"c"))
+        button.pack()
+
+def aboutkill(pp,ident):
+    if ident=="p":   
+        global aboutprogopen
+        aboutprogopen = False
+    elif ident=="c":
+        global aboutcreatopen
+        aboutcreatopen = False
+    pp.destroy()    
+        
 about_messageSaveCheck = "Do you want to save your current project first?"
 res_go=False
 cap_go=False
@@ -16,32 +59,48 @@ dip_coords=[]
 wire_coords=[]
 
 def insertResistor():
+    clearall()
     global res_go
-    res_go= not res_go
-    global res_coords
-    res_coords = []    
-    print res_go
+    res_go= True
     
 def insertCapacitor():
+    clearall()
     global cap_go
-    cap_go= not cap_go
-    global cap_coords
-    cap_coords = []
-    print cap_coords
+    cap_go= True
     
 def insertDip():
+    clearall()    
     global dip_go
-    dip_go= not dip_go
-    global dip_coords
-    dip_coords = []
-    print dip_coords
+    dip_go= True
     
 def insertWire():
+    clearall()    
     global wire_go
-    wire_go= not wire_go
+    wire_go= True
+    
+def clearall():
+    boolsfalse()
+    coordsempty()
+    
+def boolsfalse():
+    global res_go
+    res_go=False
+    global cap_go
+    cap_go=False
+    global dip_go
+    dip_go=False
+    global wire_go
+    wire_go=False
+    
+def coordsempty():
+    global res_coords
+    res_coords=[]
+    global cap_coords
+    cap_coords=[]
+    global dip_coords
+    dip_coords=[]
     global wire_coords
-    wire_coords = []
-    print wire_coords
+    wire_coords=[]
     
 def createNew():
     pass#if fileStatus == 'unsaved':
