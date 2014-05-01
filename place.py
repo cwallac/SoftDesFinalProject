41abe8fd43a9f00ea51dee7830d41d53c35037b9
unknown <rvanderheyde@901YLX1.olin.edu>
2014-05-01 13:55:04 -0400

    place

--- place.py
+++ place.py
@@ -7,7 +7,6 @@ def placingCD(list_of_components):
 			place_component(list_of_components[i],list_of_components[i].x1,list_of_components[i].y1)
 		else: 
 		 	for j in list_of_components[i].connections:
-<<<<<<< HEAD
 		 		if j == 1: 
 					k = list_of_components[i].connections[j]
 					for b in range(len(k)):
@@ -18,10 +17,9 @@ def placingCD(list_of_components):
 						update_connections(k[b],1,list_of_components[i])
 						print k[b].connections
 						place_component(k[b],list_of_components[i].x[2],list_of_components[i].y[2])
-=======
 		 		k = list_of_components[i].connections[j]
 				place_component(list_of_components[i],k[0].x[2],k[0].y[2])
->>>>>>> 0129cdf61b311150b23add6c3b1e204ace087d98
+
 
 
 def place_component(component,x=0,y=0):
